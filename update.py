492a87a53ad819861f1be05106366531e1ab8166
Crazy Lover <122443634+shivam31635@users.noreply.github.com>
2024-02-23 10:00:06 -0800

    Update update.py

--- update.py
+++ update.py
@@ -42,7 +42,7 @@ if DATABASE_URL:
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 log_info(f'Entered upstream repo: {UPSTREAM_REPO}')
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = 'https://github.com/Dawn-India/Z-Mirror'
+    UPSTREAM_REPO = 'https://github.com/shivam31635/Z-Mirror'
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 log_info(f'Entered upstream branch: {UPSTREAM_BRANCH}')
